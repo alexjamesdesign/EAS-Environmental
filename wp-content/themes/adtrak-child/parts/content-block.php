5749d3ab91ee28a2bc204c635dbd885932e9e749
Alex James <48618516+alexjamesdesign@users.noreply.github.com>
2020-04-02 22:05:15 +0100

    More progress with main content block including USPs and icons

--- wp-content/themes/adtrak-child/parts/content-block.php
+++ wp-content/themes/adtrak-child/parts/content-block.php
@@ -4,16 +4,15 @@
 
         <div class="content-block__body">
 
-        <div class="content-block__title-container">
-        <?php if( get_field('title_icon') ): ?>
-            <div class="content-block__icon">
-                <img src="<?php the_field('title_icon'); ?>" alt="icon" />
+            <div class="content-block__title-container">
+                <?php if( get_field('title_icon') ): ?>
+                <div class="content-block__icon">
+                    <img src="<?php the_field('title_icon'); ?>" alt="icon" />
+                </div>
+                <?php endif; ?>
+                <p class="title"><?php the_field('title'); ?></p>
             </div>
-        <?php endif; ?>
-            <p class="title"><?php the_field('title'); ?></p>
-        </div>
             
-
             <?php the_field('content'); ?>
 
             <?php include locate_template('parts/contact-bar.php'); ?>
@@ -21,6 +20,32 @@
         </div>
 
         <div class="content-block__secondary">
+
+            <?php
+
+            $blocktype = get_field('image_or_usps');
+
+            if( $blocktype && in_array('usps', $blocktype) ) {
+            
+            if( have_rows('usps') ) : ?>
+
+            <ul class="content-block__usps">
+            
+                <?php while ( have_rows('usps') ) : the_row(); ?>
+
+                <li>
+                    <?php $icon = get_sub_field('icon'); if( !empty( $icon ) ): ?>
+                        <img src="<?php echo esc_url($icon['url']); ?>" alt="<?php echo esc_attr($icon['alt']); ?>" />
+                    <?php endif; ?>
+                    
+                    <span><?php the_sub_field('usp'); ?></span>
+                </li>
+
+                <?php endwhile; ?>
+
+            </ul>
+            
+            <?php endif; } ?>
 
         </div>
 
