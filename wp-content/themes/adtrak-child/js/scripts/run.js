94ad18b4038a0e9ad7ed6317049e44b18e11fc05
Alex James <48618516+alexjamesdesign@users.noreply.github.com>
2020-04-16 17:09:32 +0100

    Added animation to review star on viewport

--- wp-content/themes/adtrak-child/js/scripts/run.js
+++ wp-content/themes/adtrak-child/js/scripts/run.js
@@ -5,6 +5,37 @@
 	// add all of your code within here, not above or below
 	$(function() {
 
+		// --------------------------------------------------------------------------------------------------
+		// Animate inview
+		// --------------------------------------------------------------------------------------------------
+
+		$.fn.visible = function(partial) {
+    
+			var $t            = $(this),
+				$w            = $(window),
+				viewTop       = $w.scrollTop(),
+				viewBottom    = viewTop + $w.height(),
+				_top          = $t.offset().top,
+				_bottom       = _top + $t.height(),
+				compareTop    = partial === true ? _bottom : _top,
+				compareBottom = partial === true ? _top : _bottom;
+		  
+		  return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+	  
+		};
+
+		
+		$(window).scroll(function(event) {
+  
+			$(".animate").each(function(i, el) {
+			  var el = $(el);
+			  if (el.visible(true)) {
+				el.addClass("inview"); 
+			  } 
+			});
+			
+		});
+
 
 		// --------------------------------------------------------------------------------------------------
 		// Back to top
--- wp-content/themes/adtrak-child/parts/review-bar.php
+++ wp-content/themes/adtrak-child/parts/review-bar.php
@@ -7,11 +7,11 @@
         <p>Highly Recommended</p>
 
         <div class="review-bar__stars">
-            <i class='fa fa-star'></i>
-            <i class='fa fa-star'></i>
-            <i class='fa fa-star'></i>
-            <i class='fa fa-star'></i>
-            <i class='fa fa-star'></i>
+            <i class='fa fa-star animate star-one'></i>
+            <i class='fa fa-star animate star-two'></i>
+            <i class='fa fa-star animate star-three'></i>
+            <i class='fa fa-star animate star-four'></i>
+            <i class='fa fa-star animate star-five'></i>
         </div>
 
         <a class="btn btn--noir" href="https://www.google.com/search?rlz=1C5CHFA_enGB891NO892&sxsrf=ALeKk03-WVHwlmPNUXfjrv7vVIC99o5sQQ%3A1586077409186&ei=4Z6JXpj4CsH7kwXd66uwAg&q=essex+auto+salvage+google+reviews&oq=essex+auto+salvage+google+reviews&gs_lcp=CgZwc3ktYWIQAzoECAAQRzoGCAAQBxAeOggIABAIEAcQHjoGCAAQBRAeOgYIABAIEB46BAghEApKDAgXEggxMS01NGc3NUoLCBgSBzExLThnMTRQqwZYwTJglDNoAHADeACAAWWIAegLkgEEMTkuMZgBAKABAaoBB2d3cy13aXo&sclient=psy-ab&ved=0ahUKEwjY7L3k9tDoAhXB_aQKHd31CiYQ4dUDCAw&uact=5#lrd=0x47d8faa4b16bdf29:0xd683eff8775038b4,1,,,">Google Reviews</a>
