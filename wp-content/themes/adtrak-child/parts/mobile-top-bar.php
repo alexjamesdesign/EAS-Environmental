f0d151c295cb12a2dcce17bb208f059222e04396
Alex James <48618516+alexjamesdesign@users.noreply.github.com>
2020-04-21 11:58:11 +0100

    Added icon to mobile call bar

--- wp-content/themes/adtrak-child/parts/mobile-top-bar.php
+++ wp-content/themes/adtrak-child/parts/mobile-top-bar.php
@@ -1,5 +1,7 @@
 <div class="mobile-top-bar Fixed">
 
+	<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/icon-phone.svg" alt="phone icon">
+
 	<!-- <span class="mobile-top-btn mobile-top-btn--single-number"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/icon-phone.svg" alt="phone icon"><?php do_action("ld_default", false, true); ?></span> -->
 
 	<?php do_action("ld_mobile_top", "Call us today <i class='fa fa-caret-down' aria-hidden='true'></i>"); ?>
