53c784f7e76128d0e3e09ddae03b8abac577474d
Alex James <48618516+alexjamesdesign@users.noreply.github.com>
2020-04-21 11:33:07 +0100

    Mobile top bar now has phone number dropdown instead of just one displayed

--- wp-content/themes/adtrak-child/parts/mobile-top-bar.php
+++ wp-content/themes/adtrak-child/parts/mobile-top-bar.php
@@ -1,24 +1,13 @@
 <div class="mobile-top-bar Fixed">
 
-	<span class="mobile-top-btn mobile-top-btn--single-number"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/icon-phone.svg" alt="phone icon"><?php do_action("ld_default", false, true); ?></span>
+	<!-- <span class="mobile-top-btn mobile-top-btn--single-number"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/icon-phone.svg" alt="phone icon"><?php do_action("ld_default", false, true); ?></span> -->
 
-	<?php
+	<?php do_action("ld_mobile_top", "Call us today <i class='fa fa-caret-down' aria-hidden='true'></i>"); ?>
 
-		/* 
-		---------------------------------------------------------------------------------------
-		Output the drop down trigger and drop down list
-		---------------------------------------------------------------------------------------
-
-		    <?php do_action("ld_mobile_top", "Call us today <i class='fa fa-caret-down' aria-hidden='true'></i>"); ?>
-
-			<div class="location-numbers">
-				<?php do_action("ld_default",true); ?>
-				<?php do_action("ld_list", false, "inline"); ?>
-			</div>
-
-		*/
-
-	?>
+	<div class="location-numbers">
+		<?php do_action("ld_default",true); ?>
+		<?php do_action("ld_list", false, "inline"); ?>
+	</div>
     
 	<div class="menu-btn mobile-top-btn mobile-top-btn--menu"><i class="fa fa-bars"></i> Menu</div>
 
